71d386830a8ea50a0d643462f08effd7cc0634df
sebastien <lorber.sebastien@gmail.com>
2014-01-29 12:47:21 +0100

    Refactor of pointedGraph log system

--- scripts/AppConfig.js
+++ scripts/AppConfig.js
@@ -1,20 +1,21 @@
 
 // proxy
-//$rdf.Fetcher.crossSiteProxyTemplate = "http://localhost:9000/srv/cors?url=";
+$rdf.Fetcher.crossSiteProxyTemplate = "http://localhost:9000/srv/cors?url=";
 //$rdf.Fetcher.homeServer = "http://localhost:9000/";
 //$rdf.Fetcher.crossSiteProxyTemplate = "http://data.fm/proxy?uri=";
 //$rdf.Fetcher.onlyUseProxy = false;
 
 // Level of logs in external libs.
-$rdf.PointedGraph.setLogLevel("nologs");
+$rdf.PointedGraph.setLogLevel("warning");
 
 var foafSpec = "http://xmlns.com/foaf/spec/";
 var store = new $rdf.IndexedFormula();
-$rdf.fetcher(store, 1000, true); // this makes "store.fetcher" variable available
+var fetcherTimeout = 10000; // TODO this doesn't work because of https://github.com/linkeddata/rdflib.js/issues/30
+$rdf.fetcher(store, fetcherTimeout, true); // this makes "store.fetcher" variable available
 
-//var foafDocURL = "http://bblfish.net/people/henry/card#me";
+var foafDocURL = "http://bblfish.net/people/henry/card#me";
 //var foafDocURL = "https://my-profile.eu/people/deiu/card#me";
-var foafDocURL = "https://localhost:8443/2013/backbone#me";
+//var foafDocURL = "https://localhost:8443/2013/backbone#me";
 
 
 // TODO need to add hash if needed: we do not look for primary topic anymore
